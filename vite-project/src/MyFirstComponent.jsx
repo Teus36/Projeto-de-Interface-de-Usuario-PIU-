bcd3baafa0a6db685352d7db5ed314c7a489bac4
Mateus Batista Almeida <20221101110064@ifrn.local>
2025-05-07 14:24:03 -0300

    Aula 1

---
+++ vite-project/src/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+function MyFirstComponent () {
+    
+    const nome = "Emitibi"
+
+    return (
+        <div>
+            <h1>Eae mofi</h1>
+        </div>
+    )
+}
+
+export default MyFirstComponent
