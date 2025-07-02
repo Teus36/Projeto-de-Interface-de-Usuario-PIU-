a628b6d9134459f4e826b68c7a72d879804855a4
Mateus Batista Almeida <20221101110064@ifrn.local>
2025-07-02 14:28:03 -0300

    adjustments: style improvments

--- Atividade_Lista_Tarefas/src/components/CadastrarTarefa.jsx
+++ Atividade_Lista_Tarefas/src/components/CadastrarTarefa.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 export default function CadastrarTarefa () {
     const [inputValue, setInputValue] = useState("")
     const [tarefas, setTarefas] = useState([])
+    const [prioridade, SetPrioridade] = useState({
+        nome: '',
+        prioridade: 'Alta'
+    })
 
     const handleChange = (e) => {
         setInputValue(e.target.value)
@@ -21,16 +25,22 @@ export default function CadastrarTarefa () {
         <>
             <div>
                 <h1>Lista de tarefas</h1>
-                <form onSubmit={handleSubmit}>
+                <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px'}} onSubmit={handleSubmit}>
                     <label style={{marginRight: '10px'}}>
                          <input type="text" value={inputValue} onChange={handleChange} style={{padding: '10px', borderRadius: '15px'}}/>
                     </label>
-                    <button type="submit">Enviar</button>
+                    <button style={{width: '25%', border: '2px solid black'}} type="submit">Enviar</button>
                 </form>
                 
-                <ul style={{listStyle: 'none'}}>
+                <ul style={{display: 'flex', flexDirection: 'column', gap: '10px', listStyle: 'none'}}>
                     {tarefas.map((tarefa, index) =>  (
-                        <li key={index}>{tarefa}</li>
+                        <li style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0px 15px 0px 15px', border: '1px solid black', borderRadius: '10px', width: '25em', height: '5em'}} 
+                        key={index}>{tarefa}
+                        <div>
+                            <button>🔼</button>
+                            <button>🔽</button>
+                        </div>
+                        </li>
                         ))}
                 </ul>
             </div>
